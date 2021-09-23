19fb42a7c176979c31dfbea1a1f57616852c08b8
Kevelly <kevelly133@gmail.com>
2021-09-22 21:59:23 -0300

    perf: alteração na reserva de datas

---
+++ src/Model/entities/exceptions/DomainException.java
@@ -0,0 +1,11 @@
+package Model.entities.exceptions;
+
+public class DomainException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	
+	public DomainException (String msg ) {
+		super (msg);
+	}
+	
+}
